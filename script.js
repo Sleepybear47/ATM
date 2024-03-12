da60954b1c58f7d5f0b5ef00efc7758ac0f9afcc
Sleepybear47 <alirezakazemi47@gmail.com>
2024-03-12 21:55:52 +0330

    edit project

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 "use strict";
-
-// const input = 360000;
-// let numOfFive, numOfTen, numOfOne;
-// let reminder, reminder_two, reminder_three;
-// if (input > 50000) {
-//   numOfFive = Math.floor(input / 50000);
-//   reminder = numOfFive % 50000;
-// } else if (reminder >= 10000) {
-//   numOfTen = Math.floor(reminder / 10000);
-//   reminder_two = numOfTen % 10000;
-// } else if (reminder_two >= 1000) {
-//   numOfOne = Math.floor (reminder_two / 1000) ;
-//   reminder_three = numOfOne % 1000;
-// } else {
-//   console.log(" Fuck ");
-// }
-
-// console.log(`you have ${numOfFive} fifty , ${numOfTen} ten , ${numOfOne} One`);
- 
-// *************************************************************************************
-
 const input = +prompt("Enter your number :");
 let numOfFive, numOfTen, numOfOne;
 let reminder = input, reminder_two, reminder_three;
